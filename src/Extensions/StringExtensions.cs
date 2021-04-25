2e425ccc0dfb13a156e9875b1a60986ae90ec9bb
wangkanai <sarin@wangkanai.com>
2021-04-25 12:34:01 +0700

    add string extension method

---
+++ src/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Wangkanai.Runtime.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrWhiteSpace(this string input)
+            => string.IsNullOrWhiteSpace(input);
+
+        public static bool IsUnicode(this string input)
+        {
+            var acciiBytesCount   = Encoding.ASCII.GetByteCount(input);
+            var unicodeBytesCount = Encoding.UTF8.GetByteCount(input);
+            return acciiBytesCount != unicodeBytesCount;
+        }
+    }
+}
---
+++ test/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using Wangkanai.Runtime.Extensions;
+
+using Xunit;
+
+namespace Wangkanai.Runtime.Tests.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void Mixed()
+        {
+            Assert.True("sarinสาริน".IsUnicode());
+        }
+
+        [Fact]
+        public void AsciiOnly()
+        {
+            Assert.False("123".IsUnicode());
+            Assert.False("abc".IsUnicode());
+            Assert.False("ABC".IsUnicode());
+        }
+
+        [Fact]
+        public void UnicodeOnly()
+        {
+            Assert.True("สาริน".IsUnicode());
+        }
+
+        [Fact]
+        public void DigitOnly()
+        {
+            Assert.False("123".IsUnicode());
+        }
+
+        [Fact]
+        public void AlphabetOnly()
+        {
+            Assert.False("abc".IsUnicode());
+            Assert.False("ABC".IsUnicode());
+        }
+    }
+}
