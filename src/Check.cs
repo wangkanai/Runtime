b64899393762dfc83d1e868159e3fcf60412c8dc
wangkanai <sarin@wangkanai.com>
2021-05-05 08:23:08 +0700

    add string method extensions

--- src/Check.cs
+++ src/Check.cs
@@ -28,5 +28,14 @@ namespace Wangkanai.Runtime
             => value.IsNullOrEmpty()
                    ? throw new ArgumentException($"{parameterName} can not be null or empty!", parameterName)
                    : value;
+
+        public static bool NotLessThan(int value, int expected, [InvokerParameterName] [NotNull] string parameterName)
+            => (value < expected)
+                   ? throw new ArgumentException($"{parameterName} argument can not be bigger than given string's length!")
+                   : value < expected;
+        public static bool NotMoreThan(int value, int expected, [InvokerParameterName] [NotNull] string parameterName)
+            => (value > expected)
+                   ? throw new ArgumentException($"{parameterName} argument can not be smaller than given string's length!")
+                   : value > expected;
     }
 }
--- src/Extensions/StringExtensions.cs
+++ src/Extensions/StringExtensions.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+
 using JetBrains.Annotations;
 
 namespace Wangkanai.Runtime.Extensions
@@ -72,8 +73,88 @@ namespace Wangkanai.Runtime.Extensions
             return match.Success ? match : Match.Empty;
         }
 
+
+        public static string Left(this string input, int length)
+        {
+            Check.NotNull(input, nameof(input));
+            Check.NotLessThan(input.Length, length, nameof(length));
+
+            return input.Substring(0, length);
+        }
+
+        public static string Right(this string input, int length)
+        {
+            Check.NotNull(input, nameof(input));
+            Check.NotMoreThan(input.Length, length, nameof(length));
+
+            return input.Substring(input.Length - length, length);
+        }
+
         public static string RemoveAll(this string source, params string[] strings)
             => strings.Aggregate(source, (current, value)
                                      => current.Replace(value, "", StringComparison.Ordinal));
+
+        public static string RemovePostFix(this string value, params string[] postfixes)
+        {
+            if (value is null)
+                return null;
+            if (value.IsNullOrEmpty())
+                return string.Empty;
+            if (postfixes.IsNullOrEmpty())
+                return value;
+
+            foreach (var postfix in postfixes)
+                if (value.EndsWith(postfix))
+                    return value.Left(value.Length - postfix.Length);
+
+            return value;
+        }
+
+        public static string RemovePreFix(this string value, params string[] prefixes)
+        {
+            if (value is null)
+                return null;
+            if (value.IsNullOrEmpty())
+                return string.Empty;
+            if (prefixes.IsNullOrEmpty())
+                return value;
+
+            foreach (var prefix in prefixes)
+                if (value.StartsWith(prefix))
+                    return value.Right(value.Length - prefix.Length);
+
+            return value;
+        }
+
+        public static T ToEnum<T>(this string value, bool ignoreCase = true) where T : struct
+        {
+            Check.NotNull(value, nameof(value));
+
+            return (T) Enum.Parse(typeof(T), value, ignoreCase);
+        }
+
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (value is null)
+                return null;
+
+            return value.Length <= maxLength
+                       ? value
+                       : value.Left(maxLength);
+        }
+
+        public static string TruncateWithPostfix(this string value, int maxLength, string postfix = "...")
+        {
+            if (value is null)
+                return null;
+            if (value.IsNullOrEmpty() || maxLength == 0)
+                return string.Empty;
+            if (value.Length <= maxLength)
+                return value;
+            if (maxLength <= postfix.Length)
+                return postfix.Left(maxLength);
+
+            return value.Left(maxLength - postfix.Length) + postfix;
+        }
     }
 }
