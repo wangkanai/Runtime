933dade071d2f22b28c95a07f204ca853e7becb5
wangkanai <sarin@wangkanai.com>
2021-05-08 21:55:10 +0700

    update enum method extensions

--- src/Extensions/EnumExtensions.cs
+++ src/Extensions/EnumExtensions.cs
@@ -6,25 +6,17 @@ namespace Wangkanai.Runtime.Extensions
 {
     public static class EnumExtensions
     {
-        public static bool Contains<T>(this string agent, T flags)
-            where T : Enum
+        public static bool Contains<T>(this string agent, T flags) where T : Enum
             => EnumValues<T>.TryGetSingleName(flags, out var value) && value != null
                    ? agent.Contains(value, StringComparison.Ordinal)
-                   : flags.GetFlags().Any(item => agent.Contains(ToStringInvariant<T>(item), StringComparison.Ordinal));
+                   : flags.GetFlags()
+                          .Any(item => agent.Contains(ToStringInvariant(item), StringComparison.Ordinal));
 
-        public static string ToStringInvariant<T>(this T value)
-            where T : Enum
+        public static string ToStringInvariant<T>(this T value) where T : Enum
             => EnumValues<T>.GetName(value);
 
-        public static IEnumerable<T> GetFlags<T>(this T value)
-            where T : Enum
-        {
-            var values = EnumValues<T>.Values;
-
-            foreach (var item in values)
-                if (value.HasFlag(item))
-                    yield return item;
-        }
+        public static IEnumerable<T> GetFlags<T>(this T value) where T : Enum
+            => EnumValues<T>.Values.Where(item => value.HasFlag(item));
     }
 
     public static class EnumValues<T> where T : Enum
@@ -41,9 +33,12 @@ namespace Wangkanai.Runtime.Extensions
         }
 
         public static string GetName(T value)
-            => Names.TryGetValue(value, out var result)
-                   ? result
-                   : string.Join(',', value.GetFlags().Select(x => Names[x]));
+        {
+            var flags = value.GetFlags().Select(x => Names[x]);
+            return Names.TryGetValue(value, out var result)
+                       ? result
+                       : string.Join(',', flags);
+        }
 
         public static bool TryGetSingleName(T value, out string? result)
             => Names.TryGetValue(value, out result);
