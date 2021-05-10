9304092c70d7132fc4b791b23d027baf44eae74d
wangkanai <sarin@wangkanai.com>
2021-05-10 22:19:22 +0700

    update enumerable

---
+++ src/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wangkanai.Runtime.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> list)
+            => list is null || !list.Any();
+
+        public static bool HasDuplicates<T, TProp>(this IEnumerable<T> list, Func<T, TProp> selector)
+            => list.Any(t => !new HashSet<TProp>().Add(selector(t)));
+    }
+}
