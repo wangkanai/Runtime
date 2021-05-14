e7a6be06007ec6b3e1232e43785fd1ab81948418
wangkanai <sarin@wangkanai.com>
2021-05-14 14:45:41 +0700

    enum get description

--- src/Extensions/EnumExtensions.cs
+++ src/Extensions/EnumExtensions.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace Wangkanai.Runtime.Extensions
 {
     public static class EnumExtensions
     {
+        public static string ToStringInvariant<T>(this T value) where T : Enum
+            => EnumValues<T>.GetName(value);
+        
         public static bool Contains<T>(this string agent, T flags) where T : Enum
             => EnumValues<T>.TryGetSingleName(flags, out var value) && value != null
                    ? agent.Contains(value, StringComparison.Ordinal)
                    : flags.GetFlags()
                           .Any(item => agent.Contains(ToStringInvariant(item), StringComparison.Ordinal));
-
-        public static string ToStringInvariant<T>(this T value) where T : Enum
-            => EnumValues<T>.GetName(value);
-
+        
         public static IEnumerable<T> GetFlags<T>(this T value) where T : Enum
             => EnumValues<T>.Values.Where(item => value.HasFlag(item));
+
+        public static string GetDescription(this Enum generic)
+        {
+            var type   = generic.GetType();
+            var member = type.GetMember(generic.ToString());
+            if (member != null && member.Length > 0)
+            {
+                var attributes = member[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+                if (attributes != null && attributes.Count() > 0)
+                    return ((DescriptionAttribute) attributes.ElementAt(0)).Description;
+            }
+
+            return generic.ToString();
+        }
     }
 }
