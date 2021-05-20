f4f7d3beb3af1e3b6857748a2240e28fcd5467b6
wangkanai <sarin@wangkanai.com>
2021-05-20 14:24:09 +0700

    add all hashing crypto

--- src/Cryptography/Hash.cs
+++ src/Cryptography/Hash.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Security.Cryptography;
 
 using JetBrains.Annotations;
@@ -7,16 +8,46 @@ namespace Wangkanai.Cryptography
 {
     public static class Hash
     {
-        public static string ToMd5([NotNull] this string value)
+        public static string HashMd5([NotNull] this string value)
+            => MD5.Create()
+                  .ComputeHash(value.GetAsciiBytes())
+                  .HashDataToString();
+
+        public static string HashSha512([NotNull] this string value)
+            => SHA512.Create()
+                     .ComputeHash(value.GetAsciiBytes())
+                     .HashDataToString();
+
+        public static string HashSha384([NotNull] this string value)
+            => SHA384.Create()
+                     .ComputeHash(value.GetAsciiBytes())
+                     .HashDataToString();
+
+        public static string HashSha256([NotNull] this string value)
+            => SHA256.Create()
+                     .ComputeHash(value.GetAsciiBytes())
+                     .HashDataToString();
+
+        [Obsolete]
+        public static string HashSha1([NotNull] this string value)
+            => SHA1.Create()
+                   .ComputeHash(value.GetAsciiBytes())
+                   .HashDataToString();
+
+        #region Internal
+
+        private static byte[] GetAsciiBytes([NotNull] this string value)
+            => Encoding.ASCII.GetBytes(value);
+
+        private static string HashDataToString([NotNull] this byte[] data)
         {
-            using var md5     = MD5.Create();
-            var       input   = Encoding.ASCII.GetBytes(value);
-            var       data    = md5.ComputeHash(input);
-            var       builder = new StringBuilder();
+            var builder = new StringBuilder();
             foreach (var index in data)
                 builder.Append(index.ToString("x2"));
 
             return builder.ToString();
         }
+
+        #endregion
     }
 }
